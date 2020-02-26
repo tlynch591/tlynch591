987d46f6c32c1ce7d57b4d8cc3753a0c83031755
Tristan Lynch <tristanlynch360@gmail.com>
2020-02-25 18:20:35 -0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//Student ID#0587591
+//Date:02/24/2020
+//Example 01 - Displays hello world to the console
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // create a variable to store a message
+    string message ="Hello C++ World!";
+
+    // display a message to the console
+    cout << "Hello, World! \" Hi" << endl;
+
+    return 0;
+}
+//Tristan Lynch
